e544e9a319d079dc218ff173474132da817c1b41
Christopher Gutteridge <totl@soton.ac.uk>
2019-01-21 16:06:05 +0000

    linked values in indexcard to browse

--- js/site.js
+++ js/site.js
@@ -549,28 +549,35 @@ Vue.component("summary-card", {
 });
 
 Vue.component("field-value", {
-    props: ["typedValue"],
+    props: {"typedValue": Object, "linkValue":{type:Boolean,default:true}},
     render: function (createElement) {
         var rendered_value;
         var classList = ["field-value"];
-        if (!this.typedValue.value) {
+        if (!this.typedValue.value || (this.typedValue.field.multiple===true&&this.typedValue.value.length==0)) {
             rendered_value = "unspecified";
             classList.push( "field-null" );
-        } else {
-            if (this.typedValue.field.multiple === true) {
-                rendered_value = [];
-                for (var i = 0; i < this.typedValue.value.length; ++i) {
-                    single_value = this.typedValue.value[i];
-                    if (rendered_value.length) {
-                        rendered_value.push(", ");
-                    }
-                    rendered_value.push(single_value);
+        } else if (this.typedValue.field.multiple === true) {
+            rendered_value = [];
+            for (var i = 0; i < this.typedValue.value.length; ++i) {
+                if (rendered_value.length) {
+                    rendered_value.push(", ");
                 }
-            } else {
-                rendered_value = this.typedValue.value;
+                rendered_value.push( this.renderSingleValue( createElement, this.typedValue.value[i] ) );
             }
+        } else {
+            rendered_value = [this.renderSingleValue( createElement, this.typedValue.value )];
         }
         return createElement("div", {class: classList }, rendered_value);
+    },
+    methods: {
+        renderSingleValue( createElement, value ) {
+            var rvalue = value;
+            if( this.linkValue ) {
+                var path = "/browse/" + this.typedValue.field.id + "/" + rvalue;
+                rvalue = createElement( "router-link", {attrs:{to: path}}, rvalue );
+            }
+            return rvalue;
+        }
     }
 });
 
