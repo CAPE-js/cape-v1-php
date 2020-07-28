cc2987a560cc69ca012cde8ef6428d7f66268734
totl <totl@soton.ac.uk>
2020-07-28 22:23:41 +0100

    now with actual data

--- dynamic/gitinfo.php
+++ dynamic/gitinfo.php
@@ -1,19 +1,28 @@
 <?php
 
-$branch = "???";
+$data = array(
+	"branch"=>"?",
+	"commit_user"=>"?",
+	"commit_time"=>"?",
+	"commit_id"=>"?",
+	"commit_comment"=>"?",
+);
+
 $branch_out = read_cmd( 'git branch' );
 if( preg_match( "/^\* (.*)/m", $branch_out, $matches ) ) {
-	print_r( $matches );
-} 
+	$data["branch"] = $matches[1];
+}
+
+
 $log_out = read_cmd( 'git log -1' );
+if( preg_match( "/commit\s+([^\n]+)\nAuthor:\s*([^\n]+)\nDate:\s*([^\n]+)\n((.|\n)*)/", $log_out, $matches ) ){
+	$data["commit_id"] = $matches[1];
+	$data["commit_user"] = $matches[2];
+	$data["commit_time"] = strtotime($matches[3]);
+	$data["commit_comment"] = trim($matches[4]);
+}
 
-print_r( $status );
-print "--\n";
-print "--\n";
-print "--\n";
-print_r( $log );
-
-
+print "gitinfo = ".json_encode( $data );
 exit(0);
 
 function read_cmd( $cmd ) {
