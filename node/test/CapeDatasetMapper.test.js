6e70b06b640f7516a99f874512faa56f03c0642c
David Pepper <dp11g14@soton.ac.uk>
2021-10-13 10:35:32 +0100

    Tweaked wording of unit tests

--- node/test/CapeDatasetMapper.test.js
+++ node/test/CapeDatasetMapper.test.js
@@ -1,45 +1,63 @@
-const expect = require('chai').expect;
-const cloneDeep = require('lodash').cloneDeep;
-const CapeDemoConfig = require('../CapeDemoConfig');
-const CapeDatasetMapper = require('../CapeDatasetMapper');
-const CapeValidationError = require( '../CapeValidationError' );
+const expect = require("chai").expect;
+const cloneDeep = require("lodash").cloneDeep;
+const CapeDemoConfig = require("../CapeDemoConfig");
+const CapeDatasetMapper = require("../CapeDatasetMapper");
+const CapeValidationError = require("../CapeValidationError");
 
-describe('Constructor checks', () => {
-    it('Throw validation error when passed null dataset', () => {
-        expect( ()=>{new CapeDatasetMapper(null)} ).to.throw( CapeValidationError );
+describe("CapeDataSetMapper", () => {
+  describe("Constructor", () => {
+    it("throws validation error when passed null dataset", () => {
+      expect(() => {
+        new CapeDatasetMapper(null);
+      }).to.throws(CapeValidationError);
     });
-    it('Throw validation error when passed non-object', () => {
-        expect( ()=>{new CapeDatasetMapper(23)} ).to.throw( CapeValidationError );
+    it("throws validation error when passed non-object", () => {
+      expect(() => {
+        new CapeDatasetMapper(23);
+      }).to.throws(CapeValidationError);
     });
-    it('Throw validation error when passed dataset config with no id', () => {
-        var config = cloneDeep(CapeDemoConfig.dataset);        
-        delete config.id;
-        expect( ()=>{new CapeDatasetMapper(config)} ).to.throw( CapeValidationError );
+    it("throws validation error when passed dataset config with no id", () => {
+      var config = cloneDeep(CapeDemoConfig.dataset);
+      delete config.id;
+      expect(() => {
+        new CapeDatasetMapper(config);
+      }).to.throws(CapeValidationError);
     });
-    it('Throw validation error when passed dataset config with no title', () => {
-        var config = cloneDeep(CapeDemoConfig.dataset);        
-        delete config.title;
-        expect( ()=>{new CapeDatasetMapper(config)} ).to.throw( CapeValidationError );
+    it("throws validation error when passed dataset config with no title", () => {
+      var config = cloneDeep(CapeDemoConfig.dataset);
+      delete config.title;
+      expect(() => {
+        new CapeDatasetMapper(config);
+      }).to.throws(CapeValidationError);
     });
-    it('Throw validation error when passed dataset config with no id_field', () => {
-        var config = cloneDeep(CapeDemoConfig.dataset);        
-        delete config.id_field;
-        expect( ()=>{new CapeDatasetMapper(config)} ).to.throw( CapeValidationError );
+    it("throws validation error when passed dataset config with no id_field", () => {
+      var config = cloneDeep(CapeDemoConfig.dataset);
+      delete config.id_field;
+      expect(() => {
+        new CapeDatasetMapper(config);
+      }).to.throws(CapeValidationError);
     });
-    it('Throw validation error when passed dataset config with no fields', () => {
-        var config = cloneDeep(CapeDemoConfig.dataset);        
-        delete config.fields;
-        expect( ()=>{new CapeDatasetMapper(config)} ).to.throw( CapeValidationError );
+    it("throws validation error when passed dataset config with no fields", () => {
+      var config = cloneDeep(CapeDemoConfig.dataset);
+      delete config.fields;
+      expect(() => {
+        new CapeDatasetMapper(config);
+      }).to.throws(CapeValidationError);
     });
-    it('Throw validation error when passed dataset config with empty fields', () => {
-        var config = cloneDeep(CapeDemoConfig.dataset);        
-        config.fields = [];
-        expect( ()=>{new CapeDatasetMapper(config)} ).to.throw( CapeValidationError );
+    it("throws validation error when passed dataset config with empty fields", () => {
+      var config = cloneDeep(CapeDemoConfig.dataset);
+      config.fields = [];
+      expect(() => {
+        new CapeDatasetMapper(config);
+      }).to.throws(CapeValidationError);
     });
-    it('Throw nothing when passed valid config', () => {
-        expect( ()=>{new CapeDatasetMapper(CapeDemoConfig.dataset)} ).not.to.throw();
+    it("throws nothing when passed valid config", () => {
+      expect(() => {
+        new CapeDatasetMapper(CapeDemoConfig.dataset);
+      }).not.to.throws();
     });
 
-// sort all valid fields
-// id_field exists
+    // sort all valid fields
+    // id_field exists
+  });
 });
--- node/test/CapeMapper.test.js
+++ node/test/CapeMapper.test.js
@@ -1,22 +1,34 @@
-const expect = require('chai').expect;
-const CapeMapper = require('../CapeMapper');
-const CapeValidationError = require( '../CapeValidationError' );
-const CapeDemoConfig = require('../CapeDemoConfig');
+const expect = require("chai").expect;
+const CapeMapper = require("../CapeMapper");
+const CapeValidationError = require("../CapeValidationError");
+const CapeDemoConfig = require("../CapeDemoConfig");
 
-describe('Constructor checks', () => {
-    it('Throw validation error when passed null', () => {
-        expect( ()=>{new CapeMapper(null)} ).to.throw( CapeValidationError );
+describe("CapeMapper", () => {
+  describe("Constructor", () => {
+    it("throws validation error when passed null", () => {
+      expect(() => {
+        new CapeMapper(null);
+      }).to.throw(CapeValidationError);
     });
-    it('Throw validation error when passed non-object', () => {
-        expect( ()=>{new CapeMapper(23)} ).to.throw( CapeValidationError );
+    it("throws validation error when passed non-object", () => {
+      expect(() => {
+        new CapeMapper(23);
+      }).to.throw(CapeValidationError);
     });
-    it('Throw validation error when passed site config with no datasets', () => {
-        expect( ()=>{new CapeMapper({})} ).to.throw( CapeValidationError );
+    it("throws validation error when passed site config with no datasets", () => {
+      expect(() => {
+        new CapeMapper({});
+      }).to.throw(CapeValidationError);
     });
-    it('Throw validation error when passed site config with empty datasets list', () => {
-        expect( ()=>{new CapeMapper({'datasets':[]})} ).to.throw( CapeValidationError );
+    it("throws validation error when passed site config with empty datasets list", () => {
+      expect(() => {
+        new CapeMapper({ datasets: [] });
+      }).to.throw(CapeValidationError);
     });
-    it('Throw nonthing when passed valid site config', () => {
-        expect( ()=>{new CapeMapper(CapeDemoConfig.full)} ).not.to.throw();
+    it("throws nonthing when passed valid site config", () => {
+      expect(() => {
+        new CapeMapper(CapeDemoConfig.full);
+      }).not.to.throw();
     });
+  });
 });
