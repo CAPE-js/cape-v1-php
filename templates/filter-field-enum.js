c42546eb918ad30666ac21d1436725b6b91641b9
totl <totl@soton.ac.uk>
2021-11-02 12:07:53 +0000

    moving logic into code from template as suggested in review cape-1072

--- templates/filter-field-enum.js
+++ templates/filter-field-enum.js
@@ -1,5 +1,21 @@
 Vue.component("filter-field-enum", {
     props: ["filter"],
-    template: template
+    computed: {
+        // some of the filter modes for enum have an optional alternate style
+        filterStyle: function() {
+            // default styles
+            var style = { 
+                'is': 'select', 
+                'one-of': 'multiselect' 
+            };
+            // only accept valid options
+            if( this.filter.field['style'] ) {
+                if( this.filter.field['style']['is']     == 'radio' )    { style['is'] = "radio"; }
+                if( this.filter.field['style']['one-of'] == 'checkbox' ) { style['one-of'] = "checkbox"; }
+            };
+            return style;
+        }
+    },
+    template: template,
 });
 
