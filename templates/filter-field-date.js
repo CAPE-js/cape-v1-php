81e25a7df86fcef8ed5527266f267a65ad20c695
totl <totl@soton.ac.uk>
2021-12-09 22:23:07 +0000

    moved logic out of html template per code review #1102

--- templates/filter-field-date.js
+++ templates/filter-field-date.js
@@ -1,5 +1,10 @@
 Vue.component("filter-field-date", {
     props: ["filter"],
+    computed: {
+        columns: function () {
+		return( this.filter.change_filter_mode ? 8 : 10 );
+        }
+    },
     template: template
 });
 
--- templates/filter-field-text.js
+++ templates/filter-field-text.js
@@ -1,4 +1,9 @@
 Vue.component("filter-field-text", {
     props: ["filter"],
+    computed: {
+        columns: function () {
+		return( this.filter.change_filter_mode ? 8 : 10 );
+        }
+    },
     template: template
 });
