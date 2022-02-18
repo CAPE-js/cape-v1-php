4e4bbd008a52698660b0d0bae913c3fdce6b73de
cgutteridge <christophergutteridge@gmail.com>
2022-02-18 13:12:39 +0000

    rewrote command-line wrapper to take parameters for input files.

--- node/cape-map.js
+++ node/cape-map.js
@@ -1,19 +1,33 @@
 'use strict';
 
 /*
-This is a command line tool to test the cape mapper is working. It's intended to usually be used via a Azure pipeline
- */
+This is a command line tool to test the cape mapper is working.
+It's generally intended to usually be used via a Azure pipeline.
 
+Usage:
+node cape-map.js <config.json> <dataset1> (<dataset2> ...)
+ */
 const fs = require('fs');
 const CapeMapper = require( "./lib/CapeMapper/Site" );
+
+let args = process.argv;
+args.shift(); // node
+args.shift(); // this script
+const json_config_file = args.shift();
+const tabular_files = args;
 
 const rawData = fs.readFileSync('../config.json').toString();
 let config = JSON.parse(rawData);
 
-const csvData = fs.readFileSync('../testenv/data/dataset1.csv').toString();
-
+let tabular_datasets = [];
+tabular_files.forEach( (filename)=>{
+    tabular_datasets.push( fs.readFileSync( filename ).toString() );
+})
 
 let mapper = new CapeMapper(config);
-const siteData = mapper.generate({ "testenv":[csvData]});
-console.log( JSON.stringify(siteData,{},4 ));
+const first_dataset_id = config['datasets'][0]['id'];
 
+const siteData = mapper.generate({ "testenv":tabular_datasets});
+
+// Pretty print the site JSON file to STDOUT
+console.log( JSON.stringify(siteData,{},4 ));
